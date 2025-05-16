4993770d45974563c34d9e6775fe149a580e3065
bogdan.marcu <bogdan.marcu@tss-yonder.com>
2025-05-16 09:46:54 +0300

    add the account cloud formation tempaltes

--- command.sh
+++ command.sh
@@ -2,4 +2,12 @@ aws cloudformation deploy \
   --template-file cloud-formation.yaml \
   --stack-name IamCrossAccountSetup \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides NewAccountId=
+  --parameter-overrides NewAccountId=NEW_ACCOUNT_ID
+
+aws cloudformation deploy \
+    --template-file maintainer-role.yml \
+    --stack-name MaintainerAccessRole \
+    --parameter-overrides AdminAccountId=MAIN_ACCOUNT_ID \
+    --capabilities CAPABILITY_NAMED_IAM
+
+
